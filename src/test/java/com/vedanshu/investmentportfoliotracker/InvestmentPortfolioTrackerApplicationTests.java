e583a2f96a7ac56f494b10daafe5ca1a2cf09cf2
Vedanshu Garg <garg.vedanshu04@gmail.com>
2026-03-13 23:11:24 +0530

    chore: initial setup

---
+++ src/test/java/com/vedanshu/investmentportfoliotracker/InvestmentPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedanshu.investmentportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
